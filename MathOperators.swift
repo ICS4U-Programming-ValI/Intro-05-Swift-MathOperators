c566f455d552f352618b253357dfe251cfa0fc37
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2025-02-10 15:41:37 +0000

    addedMathOps.swift

---
+++ MathOperators.swift
@@ -0,0 +1,31 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Val I on 2025-02-10.
+//  Version 1.0
+//  Copyright (c) 2025 Val I. All rights reserved.
+//
+//  This program demonstrates basic math operations such as addition, subtraction,
+//  multiplication, division, exponents, and square roots.
+//
+
+import Foundation
+
+// Addition
+print("4 + 2 = \(4.0 + 2.0)")
+// Subtraction
+print("10 - 3 = \(10.0 - 3.0)")
+// Multiplication
+print("3 * 5 = \(3.0 * 5.0)")
+// Division with integers
+print("9 / 3 = \(9 / 3)")
+// Division with floats
+print("2 / 3 = \(Float(2) / Float(3))")
+// Division with doubles
+print("2 / 3 = \(2.0 / 3.0)")
+// Exponents to the power of 2
+print("2^2 = \(pow(2.0, 2))")
+// Exponents to the power of 3
+print("2^3 = \(pow(2.0, 3))")
+// Square Root
+print("√16 = \(sqrt(16.0))")
